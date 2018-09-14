fa2c00f95e7154c7789e6f41510f8b98cf6086ca
Beta (Ben) Anggoro <ben@benworkandplay.com>
2018-09-14 14:34:47 +0700

    Fixed mobile nav

--- src/css/globalStyles.js
+++ src/css/globalStyles.js
@@ -2,9 +2,9 @@ import { injectGlobal } from 'emotion';
 import WebFont from 'webfontloader';
 
 WebFont.load({
-google: {
-    families: ['Assistant|300|800']
-}
+    google: {
+        families: ['Assistant|300|800']
+    }
 });
 
 injectGlobal`
@@ -26,6 +26,36 @@ injectGlobal`
 
     #app {
         position: relative;
+    }
+
+    .navbar {
+        position: static;
+    }
+
+    .navbar-toggler {
+        position: absolute;
+        top: 18px;
+        right: 18px;
+    }
+
+    @media (max-width: 767px) {
+        .form-inline {
+            width: 100%;
+        }
+
+        .form-inline .form-control {
+            display: inline-block;
+            vertical-align: middle;
+            width: calc(100% - 80px);
+            margin-right: 5px;
+        }
+    }
+    @media (max-width: 767px) {
+        .form-inline .form-group {
+            margin-bottom: 0;
+            width: auto;
+            vertical-align: middle;
+        }
     }
 
 `;
